f2d3761804dd05035579af5c109af952ed168bc6
Andrew <andrew.koteng@gmail.com>
2018-07-05 13:42:30 +0300

    .

--- database/seeds/GymLocationsTableSeeder.php
+++ database/seeds/GymLocationsTableSeeder.php
@@ -11,6 +11,6 @@ class GymLocationsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\GymLocation::class, 10)->create();
+        factory(App\GymLocation::class, 20)->create();
     }
 }
